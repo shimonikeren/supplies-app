5cadec3b98957ed6d7782150ab0f05afb0d6c870
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-02 20:10:20 -0700

    deploy

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ USE supplies_db;
 
 INSERT INTO supplies (supply) VALUES ('Printer Paper');
 INSERT INTO supplies (supply) VALUES ('Ink');
-INSERT INTO supplies (supply) VALUES ('Highlighters');
+INSERT INTO supplies (supply) VALUES ('Ink');
 INSERT INTO supplies (supply, purchased) VALUES ('Pencils', true);
