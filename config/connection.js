78edeef228099000fe3e3f9ee022f98cae32e46a
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-02 19:22:37 -0700

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: "8889",
-  user: "root",
-  password: "root",
-  database: "supplies_db"
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT || "8889",
+  user: process.env.MYSQL_USER || "root",
+  password:process.env.MYSQL_PASSWORD|| "root",
+  database: process.env.MYSQL_DB || "supplies_db"
 });
 
 connection.connect(function(err) {
