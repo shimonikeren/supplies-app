c6e8aa870822983fe59f6d55555861ae9bf1006c
Keren Shimoni <kerenshimoni6@gmail.com>
2018-05-02 20:30:14 -0700

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST || "localhost",
-  // port: process.env.MYSQL_PORT || "8889",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD|| "root",
-  database: process.env.MYSQL_DB || "supplies_db"
-});
+// var connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST || "localhost",
+//   // port: process.env.MYSQL_PORT || "8889",
+//   user: process.env.MYSQL_USER || "root",
+//   password: process.env.MYSQL_PASSWORD|| "root",
+//   database: process.env.MYSQL_DB || "supplies_db"
+// });
+
+
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "supplies_db"
+  })
+}
 
 connection.connect(function(err) {
   if (err) {
@@ -16,5 +30,6 @@ connection.connect(function(err) {
 
   console.log("connected as id " + connection.threadId);
 });
+
 
 module.exports = connection;
